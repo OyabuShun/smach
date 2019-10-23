dde092d75b925cfb59f325e149ca0130853c1bd0
OyabuShun <oyabu.shun961@mail.kyutech.jp>
2019-10-24 01:18:37 +0900

    Add error_handling_state.py

---
+++ scripts/error_handling_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from geometry_msgs.msg import PoseStamped
+from smach import State
+from denso_state_behavior import *
+from denso_state_smach import *
+
+class ErrorHandlingState(State):
+    """define ErrorHandlingState state"""
+
+    def __init__(self):
+        State.__init__(self, outcomes=['success', 'failed'])
+
+    def execute(self, userdata):
+        init_moving_obj = MovingBehavior(InitialState._get_initial_pose())
+
+        ret = init_moving_obj.execute_impl()
+        if ret == 'failed':
+            return 'failed'
+
+        return 'success'
